c60d523f932e05417b12a516c0b59144e51c8a85
Kauã Cassiano <87822196+KhawanC@users.noreply.github.com>
2022-06-11 21:16:09 -0300

    refinando

--- hooks/src/componentes/Componente8.jsx
+++ hooks/src/componentes/Componente8.jsx
@@ -1,24 +1,45 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const Componente8 = () => {
 
     const [listaTarefas, setListaTarefas] = useState(() => {return []})
     const [tarefa, setTarefa] = useState(() => {return ''})
+    const [status, setStatus] = useState(() => {return ''})
 
     const idTarefa = useRef(0)
 
     function adicionarTarefa() {
-        setListaTarefas(e => {return [...e, {id: idTarefa.current, texto: tarefa}]})
-        idTarefa.current++
-        setTarefa('')
+        let textIgual
+        listaTarefas.forEach(e => {
+            if(tarefa === e.texto) {
+                textIgual = true
+            }
+        });
+        if (tarefa === '') {
+            setStatus(e => {return <p>Status: <span className='statsVerm'>Item vazio!</span></p>})
+        } else if(textIgual === true) {
+            setStatus(e => {return <p>Status: <span className='statsVerm'>Item repetido!</span></p>})
+        } else {
+            setStatus(e => {return <p>Status: <span className='statsVerde'>Item criado!</span></p>})
+            setListaTarefas(e => {return [...e, {id: idTarefa.current, texto: tarefa}]})
+            idTarefa.current++
+            setTarefa('')
+        }
+        
     }
 
     function limparTarefas() {
-        setListaTarefas(e => {return []})
-        idTarefa.current = 0
+        if(listaTarefas.length === 0) {
+            setStatus(e => {return <p>Status: <span className='statsAma'>Não há tarefas!</span></p>})
+        } else {
+            setStatus(e => {return <p>Status: <span className='statsAma'>Itens deletados!</span></p>})
+            setListaTarefas(e => {return []})
+            idTarefa.current = 0
+        }
     }
 
     function removerTarefa(id) {
+        setStatus(e => {return <p>Status: <sapn className='statsAma'>Item  removido!</sapn></p>})
         const filtList = listaTarefas.filter(tarefa => tarefa.id !== id)
         setListaTarefas(filtList)
     }
@@ -35,11 +56,12 @@ const Componente8 = () => {
                     <button onClick={adicionarTarefa}>Adicionar</button>
                     <button onClick={limparTarefas}>Limpar</button>
                 </div>
+                {status}
                 <hr className='hr'/>
                 <p>Tarefas: </p>
                 {listaTarefas.map(e => {
-                    return <div className='componente2'>
-                        <span key={e.id}>{e.texto}</span>
+                    return <div className='componente2' key={e.id}>
+                        <span>{e.texto}</span>
                         <button onClick={() => {removerTarefa(e.id)}}>remover</button>
                     </div>
                 })}
